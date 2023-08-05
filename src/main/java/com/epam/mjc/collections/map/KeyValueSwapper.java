d04bb6bc9b694b956e04dee061710cc4d4fae748
shapovalovaaa <nastyashpovalova70@gmail.com>
2023-08-05 19:12:12 +0300

    feat: solve task

--- src/main/java/com/epam/mjc/collections/map/KeyValueSwapper.java
+++ src/main/java/com/epam/mjc/collections/map/KeyValueSwapper.java
@@ -1,8 +1,14 @@
 package com.epam.mjc.collections.map;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class KeyValueSwapper {
     public Map<String, Integer> swap(Map<Integer, String> sourceMap) {
+        Map<String, Integer> newMap = new HashMap<>();
+        for (Map.Entry<Integer, String> pair : sourceMap.entrySet()) {
+            newMap.merge(pair.getValue(), pair.getKey(), Integer::min);
+        }
+        return newMap;
     }
 }
--- src/main/java/com/epam/mjc/collections/map/WordRepetitionMapCreator.java
+++ src/main/java/com/epam/mjc/collections/map/WordRepetitionMapCreator.java
@@ -1,8 +1,20 @@
 package com.epam.mjc.collections.map;
 
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class WordRepetitionMapCreator {
     public Map<String, Integer> createWordRepetitionMap(String sentence) {
+        Map<String, Integer> wordsFrequency = new HashMap<>();
+
+        if (!sentence.isEmpty()) {
+            List<String> words = Arrays.stream(sentence.toLowerCase().replaceAll("\\p{Punct}", "").split(" "))
+                    .collect(Collectors.toList());
+            Set<String> uniqueWords = new HashSet<>(words);
+            for (String word : uniqueWords) {
+                wordsFrequency.put(word, Collections.frequency(words, word));
+            }
+        }
+        return wordsFrequency;
     }
 }
